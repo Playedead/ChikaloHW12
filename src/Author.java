8481695cac78665d8fd651147eefc2978e538c38
playedead <awwii94@gmail.com>
2023-11-13 18:17:18 +0400

    HW 12

---
+++ src/Author.java
@@ -0,0 +1,20 @@
+public class Author {
+
+  String name;
+  String surname;
+
+  public Author(String name, String surname) {
+    this.name = name;
+    this.surname = surname;
+  }
+
+  public String getAuthorName() {
+    return name;
+  }
+
+  public String getAuthorSurname() {
+    return surname;
+  }
+}
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hello worl!");
+    Author verber = new Author("Bernard", "Verber");
+    Author lem = new Author("Stanislaw","Lem");
+    Book tanatonauts = new Book("Tanatonauts", verber, 1994);
+    Book solaris = new Book("Solaris", lem, 1961);
+    System.out.println("Книга - " +tanatonauts.getBookName());
+    System.out.println("Автор - " +tanatonauts.getBookAuthor());
+    System.out.println("Год публикации - " +tanatonauts.getBookYear());
+    System.out.println();
+    System.out.println("Книга - " +solaris.getBookName());
+    System.out.println("Автор - " +solaris.getBookAuthor());
+    System.out.println("Год публикации - " +solaris.getBookYear());
+    solaris.setBookYear(1962);
+    System.out.println("Год публикации - " +solaris.getBookYear());
   }
 }
